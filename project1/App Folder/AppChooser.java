e70df4abda2ca49220e37b3f98ed3349c2a5bd82
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-10-31 21:11:58 -0400

    Includes dockerfile for app chooser and sample deployment file

---
+++ project1/App Folder/AppChooser.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class AppChooser
+{
+    public static void main(String[] args)
+    {
+        Scanner inScan = new Scanner(System.in);
+        System.out.println("Welcome to the Big Data Processing Application");
+        System.out.println("Please type the number that corresponds to your choice");
+        System.out.println("1. Apache Hadoop");
+        System.out.println("2. Apache Spark");
+        System.out.println("3. Jupyter Notebook");
+        System.out.println("4. Sonar Qube and SonarScanner");
+        System.out.println("What is your choice?");
+        String answer = inScan.nextLine();
+
+
+        
+
+
+    }
+}
